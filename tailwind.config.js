3c45dbaae580004c7ae9ddc5e28cb420c54ada1c
controldecalidadrc50602-pixel <controldecalidadrc50602@gmail.com>
2026-02-06 10:02:57 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
-     "./**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        slate: {
-          850: '#151f32',
-        }
+        slate: { 850: '#151f32', 950: '#0a0f1a' }
       }
     },
   },
